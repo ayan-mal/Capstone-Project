1e7c7367f0a158b6c4b8d5954dcf9c71fee84594
Ayan Mal <ayan.mal201@gmail.com>
2026-06-11 11:18:21 +0530

    new commit

--- Automation_testNG/src/test/java/Base/BaseTest.java
+++ Automation_testNG/src/test/java/Base/BaseTest.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -19,9 +20,18 @@ public class BaseTest {
 
     @BeforeTest(alwaysRun = true)
     public void setup() {
+    	
+    	 ChromeOptions options = new ChromeOptions();
+
+         options.addArguments("--headless=new");
+         options.addArguments("--no-sandbox");
+         options.addArguments("--disable-dev-shm-usage");
+         options.addArguments("--window-size=1920,1080");
+
+         driver = new ChromeDriver(options);
   
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+       // driver = new ChromeDriver();
+       // driver.manage().window().maximize();
         driver.get("https://testautomationpractice.blogspot.com/");
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         js = (JavascriptExecutor) driver;
